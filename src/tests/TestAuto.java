ba3885113d0e1f1e2723479c76d651a9a2b4f8ee
Nicholas <gianfrancopr08@gmail.com>
2023-05-19 14:26:49 -0500

    Se eliminaron clases que no servían para el proyecto.

--- src/tests/TestAuto.java
+++ src/tests/TestAuto.java
@@ -4,6 +4,6 @@ import auto.Llanta;
 
 public class TestAuto {
     public static void main(String[] args) {
-        Llanta llanta1 = new Llanta(5,3.2,'s');
+        Llanta llanta1 = new Llanta(5,3.2,'H');
     }
 }
